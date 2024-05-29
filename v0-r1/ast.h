57b66b3c847c3af8580023901646b4fbf56a7070
FkjAccount123456 <yunkaijisan@qq.com>
2024-05-29 22:28:07 +0800

    Commit

---
+++ v0-r1/ast.h
@@ -0,0 +1,47 @@
+#ifndef AST_H
+#define AST_H
+
+#include "b_string.h"
+#include "b_lexer.h"
+
+typedef enum ExprType {
+  INT_AST,
+  STR_AST,
+  ID_AST,
+  BINARY_AST,
+  UNARY_AST,
+  CALL_AST,
+  BUILDLIST_AST,
+  INDEX_AST,
+} ExprType;
+
+typedef struct Expr {
+  ExprType type;
+  union {
+    long long int_ast;
+    String *str_ast;
+    struct {
+      TokenType op;
+      struct Expr *left, *right;
+    } binary_ast;
+    struct {
+      TokenType op;
+      struct Expr *expr;
+    } unary_ast;
+    struct {
+      struct Expr *func;
+      struct Expr **args;
+      int nargs;
+    } call_ast;
+    struct {
+      struct Expr **items;
+      int nitems;
+    } buildlist_ast;
+    struct {
+      struct Expr *expr;
+      struct Expr *index;
+    } index_ast;
+  };
+} Expr;
+
+#endif // AST_H
