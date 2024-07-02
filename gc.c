13e24d2e7324a8e006203912e3cb716419aebfa5
FkjAccount123456 <yunkaijisan@qq.com>
2024-07-02 19:02:51 +0800

    Tests passed & VMFrame trouble fixed

--- gc.c
+++ gc.c
@@ -93,7 +93,7 @@ void GC_collect() {
     } else if (gc.G_bases[i]->ptr == NULL) {
       // puts("NULL");
     } else {
-      printf("%llu failed to collect\n", i);
+      // printf("%llu failed to collect\n", i);
     }
     gc.G_isrefed[i] = false;
   }
